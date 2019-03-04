918e1daaaa45f1ed7300bd782c428b9d3461fcf1
Camarones <cameron_walther@yahoo.com>
2019-03-04 15:51:51 -0500

    crushed some button display

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,17 @@
 // create an array of strings to loop through
 // in this case I think I've decided on a theme of popular TV shows
-var characterArray = ["Dexter", "The Office", "How I Met Your Mother", "Silicon Valley", "Veep", "True Detective", "How I Met Your Mother", "Parks and Rec", "Community", "Family Guy", "Rick and Morty", "American Dad"]
+var tvShowArray = ["Dexter", "The Office", "How I Met Your Mother", "Silicon Valley", "Veep", "True Detective", "How I Met Your Mother", "Parks and Rec", "Community", "Family Guy", "Rick and Morty", "American Dad"]
 
 $("document").ready(function() {
 
+    function displayButtons() {
+        $("#show-buttons").empty();
+
+        for(var i = 0; i < tvShowArray.length; i++) {
+            var $showbutton = $("<button>");
+            $showbutton.text(tvShowArray[i]);
+            $("#show-buttons").append($showbutton);
+        }
+    }
+    displayButtons();
 });
