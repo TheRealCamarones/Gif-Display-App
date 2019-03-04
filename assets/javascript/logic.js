adfbd90c832eb1745938ae1b1d92e7a3123677af
Camarones <cameron_walther@yahoo.com>
2019-03-04 16:14:55 -0500

    adding new shows through input box

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,14 +4,38 @@ var tvShowArray = ["Dexter", "The Office", "How I Met Your Mother", "Silicon Val
 
 $("document").ready(function() {
 
+    function displayShowGif () {
+
+    };
+
+    // when the submit button is clicked
+    $("#submit-button").on("click", function(event) {
+        event.preventDefault();
+
+        // create a variable for the text in the input box
+        var newShow = $("#add-show").val().trim();
+
+        // push the new show into the array
+        tvShowArray.push(newShow);
+
+        // and then call our display buttons function again
+        displayButtons();
+    });
+
+    // function to create display all of the buttons in the array
     function displayButtons() {
         $("#show-buttons").empty();
 
+        // create buttons, add class and attribute to them and add them to the HTML
         for(var i = 0; i < tvShowArray.length; i++) {
             var $showbutton = $("<button>");
+            $showbutton.addClass("show-btn");
+            $showbutton.attr("show-name", tvShowArray[i]);
             $showbutton.text(tvShowArray[i]);
             $("#show-buttons").append($showbutton);
         }
-    }
+    };
+
+    // initial call to display initial buttons
     displayButtons();
 });
