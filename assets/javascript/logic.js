604ee143578417147b738790e07f031cba49eb11
Camarones <cameron_walther@yahoo.com>
2019-03-04 12:45:23 -0500

    coded out the simple HTML structure, built my array of TV shows to loop through

---
+++ assets/javascript/logic.js
@@ -0,0 +1,7 @@
+// create an array of strings to loop through
+// in this case I think I've decided on a theme of popular TV shows
+var characterArray = ["Dexter", "The Office", "How I Met Your Mother", "Silicon Valley", "Veep", "True Detective", "How I Met Your Mother", "Parks and Rec", "Community", "Family Guy", "Rick and Morty", "American Dad"]
+
+$("document").ready(function() {
+
+});
